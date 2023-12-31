9efe78904716c2d39b45e0d912253c7a5590701e
mrohadi <muhammadrohadi03@gmail.com>
2023-12-31 16:07:38 +0700

    update

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,16 @@
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
+import TabButton from "./components/TabButton";
 
 function App() {
   const listConcepts = CORE_CONCEPTS.map((concept, index) => (
     <CoreConcept {...concept} key={index} />
   ));
+
+  const handleSelect = () => {
+    console.log("Hello World - Handle selected");
+  };
 
   return (
     <div>
@@ -15,6 +20,17 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>{listConcepts}</ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Component</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
