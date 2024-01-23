87913d1ec0bc97bdf39d256840cb3a12f7ccada2
mrohadi <muhammadrohadi03@gmail.com>
2024-01-23 21:00:22 +0700

    [mrohadi - Finished auth

--- 23-auth/frontend/src/components/AuthForm.js
+++ 23-auth/frontend/src/components/AuthForm.js
@@ -1,19 +1,37 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+  json,
+  redirect,
+} from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [searchParams] = useSearchParams();
+  const data = useActionData();
+  const navigation = useNavigation();
 
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const isLogin = searchParams.get("mode") === "login";
+  const isSubmitting = navigation.state === "submitting";
 
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+
+        {data && data.errors && (
+          <ul>
+            {Object.values(data.errors).map((err) => (
+              <li key={err}>{err}</li>
+            ))}
+          </ul>
+        )}
+        {data && data.message && <p>{data.message}</p>}
+
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
@@ -23,10 +41,12 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
-            {isLogin ? 'Create new user' : 'Login'}
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+            {isLogin ? "Create new user" : "Login"}
+          </Link>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "..." : "Save"}
           </button>
-          <button>Save</button>
         </div>
       </Form>
     </>
@@ -34,3 +54,43 @@ function AuthForm() {
 }
 
 export default AuthForm;
+
+export async function authAction({ request }) {
+  const searchParam = new URL(request.url).searchParams;
+  const mode = searchParam.get("mode") || "login";
+
+  if (mode !== "login" && mode !== "signup") {
+    throw json({ message: "Unsupported mode." }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Could not authenticate user" }, { user: 500 });
+  }
+
+  const responseData = await response.json();
+  localStorage.setItem("token", responseData.token);
+
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem("expiration", expiration.toISOString());
+
+  return redirect("/");
+}
